bc6b538011b67bd751a57467d2d06079eb6a71bb
Tiago Babo <tiago@tiago-HP.(none)>
2011-10-23 00:22:21 +0100

    Adicionas validações extra ao e-mail e ao telefone do shopping.

--- app/models/shopping.rb
+++ app/models/shopping.rb
@@ -1,6 +1,8 @@
 class Shopping < ActiveRecord::Base
   
-  
   validates_presence_of :nome, :telefone, :localizacao, :email, :descricao, :coordenadas, :imagem
+  validates_numericality_of :telefone, :only_integer => true, :count => 9, :message => "só pode ter dígitos."
+  validates :telefone, :length => {:is => 9}
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :message => "inválido."
   
 end
