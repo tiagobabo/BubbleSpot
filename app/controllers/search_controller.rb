fa563563b1371e552650dfae1765877af5272fe4
Hélder <helder@helder-laptop.(none)>
2011-10-24 15:59:31 +0100

    melhorada a pesquisa. é agora possível fazer pesquisas por vários atributos.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,28 @@
 class SearchController < ApplicationController
+
+  TERMOS = [['Nome',0], ['Localização',1], ['Email',2]]
   
   def shoppings   
+    
+    
     if params[:query]
-      search = params[:query] + "%"
-      @shoppings = Shopping.where("nome like ?", search)
       
+      if params[:pesquisa] == "0"
+        search = params[:query].downcase + "%"
+        @shoppings = Shopping.where("nome like ?", search)        
+      elsif params[:pesquisa] == "1"
+        search = params[:query].downcase + "%"
+        @shoppings = Shopping.where("localizacao like ?", search)
+      elsif params[:pesquisa] == "2"
+        search = params[:query].downcase + "%"
+        @shoppings = Shopping.where("email like ?", search)        
+      end
+  
       respond_to do |format|
         format.html # shoppings.html.erb
         format.json { render :json => @shoppings }
       end
-      
+  
     else
       render "shoppings_index"
     end
