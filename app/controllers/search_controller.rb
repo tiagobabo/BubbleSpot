25a3f3007e8638c1133f22078605d303dc611196
Hélder <helder@helder-laptop.(none)>
2011-10-24 14:51:18 +0100

    adicionado retorno em json para as pesquisas de shoppings

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,12 @@ class SearchController < ApplicationController
     if params[:query]
       search = params[:query] + "%"
       @shoppings = Shopping.where("nome like ?", search)
+      
+      respond_to do |format|
+        format.html # shoppings.html.erb
+        format.json { render :json => @shoppings }
+      end
+      
     else
       render "shoppings_index"
     end
