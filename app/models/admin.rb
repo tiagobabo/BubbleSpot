b162472de1bd4ea6d381eaca8b0fbe9174041225
Tiago Babo <tiago.babo@gmail.com>
2011-10-28 22:16:46 +0100

    adiocionadas algumas traduções

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -8,8 +8,8 @@ class Admin < ActiveRecord::Base
   before_save :encrypt_password
   
   validates_confirmation_of :password, :message => "A password não coincide com a de confirmação"
-  validates_presence_of :password, :on => :create
-  validates_presence_of :email
+  validates_presence_of :password, :on => :create, :message => "O campo não pode estar vazio"
+  validates_presence_of :email, :message => "O campo não pode estar vazio"
   validates_uniqueness_of :email, :message => "O e-mail já existe"
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :message => "O e-mail é inválido"
   validates :password, :length => {:is => 6, :message => "A password tem de conter mais de 6 caracteres"}
