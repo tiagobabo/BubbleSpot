0aa12c4da473e1378b4ca60320aeb3b197897248
Tiago Babo <tiago.babo@gmail.com>
2011-11-22 13:53:48 +0000

    alterado model das lojas para melhorar a consistência da informação

--- app/models/loja.rb
+++ app/models/loja.rb
@@ -1,3 +1,10 @@
 class Loja < ActiveRecord::Base
   belongs_to :shopping
+  
+  validates_presence_of :nome, :piso, :telefone, :detalhes, :tags, :imagem, :message => "O campo tem de ser preenchido"
+  validates_numericality_of :telefone, :only_integer => true, :count => 9, :message => "O telefone só pode ter dígitos."
+  validates_numericality_of :piso, :only_integer => true,  :message => "O piso só pode ter dígitos."  
+  validates_numericality_of :telefone, :only_integer => true, :count => 9, :message => "O telefone só pode ter dígitos."
+  validates :telefone, :length => {:is => 9, :message => "O telefone tem de ter 9 dígitos"} 
+  
 end
