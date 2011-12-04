26b6af24bfaca1a69c06db8691271fbb347b423d
Bruno <bruno@bruno-K50IN.(none)>
2011-12-04 15:45:23 +0000

    calculo dos valores das promocoes, alerta com valores negativos e a apagar os campos, ao mostrar promocao que apenas tem desconto nao mostra os precos finais e iniciais

--- app/assets/javascripts/promos.js
+++ app/assets/javascripts/promos.js
@@ -8,4 +8,70 @@ $(function () {
   
 	austDay = new Date($('#year').html(), $('#month').html() - 1 , $('#day').html(), $('#hour').html(), $('#minutes').html());
 	$('#defaultCountdown').countdown({until: austDay});
+  
+  $( "#datepicker" ).datepicker();
+  
+  $( "#inicial" ).change(calculoInicial);
+  $( "#desconto" ).change(calculoInicial);
+  $( "#final" ).change(calculoInicial2);
+  
+  function calculoInicial2(){
+   if($( "#inicial").val().length == 0 && $( "#desconto").val().length > 0 && $( "#final").val().length > 0 ){
+            $( "#inicial").val($( "#final" ).val() / ( 1- ($( "#desconto" ).val()/100)));  
+      }
+      else if( $( "#inicial").val().length > 0 && $( "#final").val().length > 0){
+         var resultado = ((1-($( "#final" ).val()/$( "#inicial" ).val()))*100);
+     
+        if(resultado < 0){
+            alert("Valores Inválidos");
+            $( "#final" ).val("");
+            $( "#desconto" ).val("");
+        }
+        else{
+            $( "#desconto" ).val(resultado);
+        }
+    }
+  }
+  
+  function calculoInicial(){
+   
+    if($( "#final").val() < 0 || $( "#desconto").val() < 0 || $( "#desconto").val() > 100 || $( "#inicial").val() < 0 ){
+          alert("Valores Inválidos");
+          $( "#inicial" ).val("");
+          $( "#final" ).val("");
+          $( "#desconto" ).val("");
+    }
+    else{
+      if($( "#inicial").val().length == 0 && $( "#desconto").val().length > 0 && $( "#final").val().length > 0 ){
+            $( "#inicial").val($( "#final" ).val() / ( 1- ($( "#desconto" ).val()/100)));  
+      }
+      else if( $( "#inicial").val().length > 0 && $( "#desconto").val().length > 0){
+        var resultado = ($( "#inicial" ).val()-($( "#inicial" ).val()*$( "#desconto" ).val()/100));
+       
+        if(resultado < 0){
+            alert("Valores Inválidos");
+            $( "#final" ).val("");
+            $( "#desconto" ).val("");
+        }
+        else{
+            $( "#final" ).val(resultado);
+        }
+
+      }
+
+      else if( $( "#inicial").val().length > 0 && $( "#final").val().length > 0){
+        var resultado = ((1-($( "#final" ).val()/$( "#inicial" ).val()))*100);
+     
+        if(resultado < 0){
+            alert("Valores Inválidos");
+            $( "#final" ).val("");
+            $( "#desconto" ).val("");
+        }
+        else{
+            $( "#desconto" ).val(resultado);
+        }
+
+      }
+  }
+  }
 });
