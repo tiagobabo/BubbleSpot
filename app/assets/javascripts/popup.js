091e5693739487847e59e0cd124743154cac9bec
Bruno <bruno@bruno-K50IN.(none)>
2011-12-05 23:29:10 +0000

    js na pagina de pesquisa para as promos

--- app/assets/javascripts/popup.js
+++ app/assets/javascripts/popup.js
@@ -28,11 +28,19 @@
 		});
     
     
-        $( "#opener" ).click(function() {
+    $( "#opener" ).click(function() {
       $("#cover").show();
 
 			$( "#dialog-modal2" ).dialog( "open" );
       $( "#dialog-modal2 ul" ).css("width","196px");
+			return false;
+		});
+        
+    $( "#opener3" ).click(function() {
+      $("#cover").show();
+
+			$( "#dialog-modal3" ).dialog( "open" );
+      $( "#dialog-modal3 ul" ).css("width","196px");
 			return false;
 		});
     
