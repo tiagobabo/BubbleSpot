f007378436dda549c4890ea220a85b4e4604634d
Tiago Babo <tiago.babo@gmail.com>
2011-12-07 15:51:10 +0000

    tentar melhorar o heroku

--- config/application.rb
+++ config/application.rb
@@ -38,9 +38,6 @@ module BubbleSpot
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
     
     config.assets.initialize_on_precompile = false
 
