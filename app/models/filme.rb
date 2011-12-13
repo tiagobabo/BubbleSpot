6716dc52cb3fc835c424e8ecaafbe96b3f4cf619
Tiago Babo <tiago.babo@gmail.com>
2011-12-13 13:26:11 +0000

    check da extensão da imagem nos filmes

--- app/models/filme.rb
+++ app/models/filme.rb
@@ -3,7 +3,7 @@
 class Filme < ActiveRecord::Base
   belongs_to :shopping
   validates_presence_of :nome, :detalhes, :horarios, :imagem, :trailer, :sala, :message => "O campo tem de ser preenchido"
-  
+  validates_format_of :imagem, :with => %r{\.(png|jpg|jpeg|bmp)$}i, :message => "O formato da imagem não é válido"
   
 
 end
