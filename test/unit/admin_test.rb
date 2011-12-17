1483a750639af5fbd439adebeb5b58f31cc13f61
Tiago Babo <tiago.babo@gmail.com>
2011-12-17 18:11:04 +0000

    inicio dos testes unitários

--- test/unit/admin_test.rb
+++ test/unit/admin_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	fixtures :admins
+	test "admin_valido" do
+		admin = Admin.new(:email => "teste@teste.com", :password => "123456", 
+		:password_confirmation => "123456", :idref => "1", :tipo => "1")
+		assert admin.save
+	end
+
+	test "email_invalido" do
+		admin = Admin.new(:email => "teste", :password => "123456", 
+		:password_confirmation => "123456", :idref => "1", :tipo => "1")
+		assert admin.invalid?
+		assert !admin.save
+	end
+
+	test "password_invalida" do
+		admin = Admin.new(:email => "teste@gmail.com", :password => "123456", 
+		:password_confirmation => "12345", :idref => "1", :tipo => "1")
+
+		assert admin.invalid?
+		assert !admin.save
+	end
 end
