7ed9834cd0efa5c79f12d8f5b28a200e73141882
Hélder Moreira <heldersantos.moreira@gmail.com>
2011-12-18 15:59:51 +0000

    terminados os testes unitários.

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -2,7 +2,8 @@
 
 class Promo < ActiveRecord::Base
   belongs_to :loja  
-  # validates_presence_of :loja
+  
+  validates_presence_of :loja
   validates_presence_of :detalhes, :imagem, :dataf, :desconto, :produto, :tags, :message => "O campo tem de ser preenchido"
   validates_format_of :imagem, :with => %r{\.(png|jpg|jpeg|bmp|gif)$}i, :message => "O formato da imagem não é válido"
   
